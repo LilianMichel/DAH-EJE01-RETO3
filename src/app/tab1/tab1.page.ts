6b230a8a3c6f75dff81ed757d3f9fb0c4c2b3b73
LilianMichel <55470126+LilianMichel@users.noreply.github.com>
2020-04-24 21:46:11 -0600

    RetoEspecial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  students: any = [
+      {name:"CAMPOS AGUIRRE LILIAN MICHEL", grade: 80, nc: '16400890', active: true},
+      {name:"CASTELLANOS MORA AEXIS GERARDO", grade: 90, nc: '16400891', active: false},
+      {name:"CASTILLO CORRALES VICTOR EMANUEL", grade: 65, nc: '16400892', active: true},
+      {name:"CASTILLO FRANQUEZ MARISSA MICHELLE", grade: 40, nc: '16400893', active: false}
+    ];
+  constructor(private router: Router) {}
 
+  view(student: any): void {
+    const extras: NavigationExtras = {
+      queryParams: {
+        special: JSON.stringify(student)
+      }
+    };
+
+    this.router.navigate(['/view-student'], extras);
+  }
 }
